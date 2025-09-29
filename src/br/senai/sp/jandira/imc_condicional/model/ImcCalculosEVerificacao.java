cffd84b2005f9a294e15da4beec652ebe89e8a53
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2025-09-29 16:07:55 -0300

    atualização de lógica de classificação na tabela IMC

--- src/br/senai/sp/jandira/imc_condicional/model/ImcCalculosEVerificacao.java
+++ src/br/senai/sp/jandira/imc_condicional/model/ImcCalculosEVerificacao.java
@@ -8,6 +8,7 @@ public class ImcCalculosEVerificacao {
     String nomeDoAluno;
     double peso;
     double altura;
+    String classificação;
 
     public void receberDados(){
 
@@ -19,13 +20,15 @@ public class ImcCalculosEVerificacao {
 
         System.out.println("-----------------------------------------------");
 
+        System.out.println();
+
         System.out.print("Digite o nome do aluno: ");
         nomeDoAluno = leitor.nextLine();
 
-        System.out.print("Digite o peso do aluno(em kg): ");
+        System.out.print("Digite o peso (em kg): ");
         peso = leitor.nextDouble();
 
-        System.out.print("Digite a altura do aluno(em metros): ");
+        System.out.print("Digite a altura (em metros): ");
         altura = leitor.nextDouble();
 
         calcularImc();
@@ -38,19 +41,18 @@ public class ImcCalculosEVerificacao {
     }
     public void exibirClassificaçãoDoImc(){
 
-
-        if (imc<= 18.5){
-            System.out.println(" Status: abaixo do peso");
-        } else if (imc >= 18.6 && imc<=24.9) {
-            System.out.println("Status: Peso ideal(Parabéns)");
-        } else if (imc >= 25.0 && imc <= 29.9){
-            System.out.println("STatus: Levemente acima do peso");
-        }else if (imc >= 30.0 && imc <= 34.9){
-            System.out.println("Status: Obsidade grau I ");
-        } else if (imc >= 35.0 && imc <= 39.9){
-            System.out.println("Status: Obesidade grau II(Severa) ");
-        } else if (imc>= 40.0) {
-            System.out.println("Obesidade III(Mórbida) ");
+        if (imc<= 18.6){
+            classificação = "Abaixo do peso";
+        } else if (imc >= 18.6 && imc< 25) {
+            classificação = "Peso ideal (PARABÉNS)";
+        } else if (imc >= 25.0 && imc < 30.0){
+            classificação = "Levemente acima do peso";
+        }else if (imc >= 30.0 && imc < 35){
+            classificação = "Obesidade grau I";
+        } else if (imc >= 35.0 && imc < 40){
+            classificação = "Obesidade grau III (Severa)";
+        } else {
+            classificação = "Obesidade III (Mórbida)";
         }
 
         exibirResultadosAoUsuario();
@@ -62,6 +64,8 @@ public class ImcCalculosEVerificacao {
         System.out.println("-----RESULTADO-----");
         System.out.println("Aluno: " + nomeDoAluno);
         System.out.println("IMC Calculado: " + valorImcComDoisDecimais);
+        System.out.println("Status: " + classificação );
+        System.out.println("-------------------");
 
     }
 }
